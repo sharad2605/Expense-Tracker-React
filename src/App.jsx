c82ee7c5be86f0cf491c7881ce0d4190f02c9b62
sharad2605 <sharad26shukla@gmail.com>
2025-03-07 14:57:20 +0530

    Signup page commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Signup from './Component/SignUp/Signup'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Signup />
+        </div>
+    </>
+  )
+}
+
+export default App
